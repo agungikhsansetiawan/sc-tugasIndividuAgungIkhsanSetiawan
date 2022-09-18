ed92bb2d8da599911ba9710ebb6f46fbfcaeb244
unknown <goldenboy10122000@gmail.com>
2022-09-18 15:22:17 +0700

    Penambahan feature add, multiplication, and division

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math_function import add, mul, div
+
 
 def main():
 
@@ -5,7 +7,15 @@ def main():
     data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
 
-    print("{} {} {}".format(data_1, operator, data_2))
+    if operator == "+":
+        result = add(data_1, data_2)
+    elif operator == "*":
+        result = mul(data_1, data_2)
+    elif operator == "/":
+        result = div(data_1, data_2)
+
+
+    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
 
 if __name__ == "__main__":
